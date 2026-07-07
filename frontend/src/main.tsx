5f413a199eb32698185d84f22b426b9534efb0e7
Shade-Angel <maratsamat18@gmail.com>
2026-07-07 09:23:49 +0500

    настройка проекта, создание алиасов для импортов, очистка шаблона для работы

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { QueryProvider, ThemeProvider, RouterProvider, AuthProvider } from "@app/providers";
+import "./app/styles/global.css";
+import { AppRouter } from "@app/routes/AppRouter";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <App />
-    </StrictMode>,
-)
+        <QueryProvider>
+            <ThemeProvider>
+                <RouterProvider>
+                    <AuthProvider>
+                        <AppRouter />
+                    </AuthProvider>
+                </RouterProvider>
+            </ThemeProvider>
+        </QueryProvider>
+    </StrictMode>
+);
