e2c6693ff10e8819c9b1bb4f0c0a4c6f7cc85595
Shade-Angel <maratsamat18@gmail.com>
2026-07-23 09:41:04 +0500

    интеграция с бекендом

--- frontend/src/entities/post/model/types.ts
+++ frontend/src/entities/post/model/types.ts
@@ -1,13 +1,18 @@
 import type { IUser } from "@entities/user";
 
 export interface IPost {
-    id: string;
+    id: number;
     content: string;
     author: IUser;
     createdAt: string;
     likesCount?: number;
     commentsCount?: number;
     imagePath?: string;
+}
+
+export interface IPostCreatePayload{
+    text: string;
+    image?: File;
 }
 
 export interface IPostCreate {
